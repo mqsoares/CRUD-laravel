aeeab80fa79a2b73c16af5736452a38a33198bed
Miquéias Soares <mqseraos@gmail.com>
2024-05-24 00:06:45 -0300

    feat: ajust container yield

--- resources/views/clients/layout.blade.php
+++ resources/views/clients/layout.blade.php
@@ -14,7 +14,9 @@
             <small class="text-light">by Miqueias Soares / mqseraos@gmail.com</small>
         </div>
     </div>
-    @yield('content')
+    <div class="container my-5">
+        @yield('content')
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
